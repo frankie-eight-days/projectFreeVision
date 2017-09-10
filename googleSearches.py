2ef644dcc70c2ea3498447456c85c572bf3f2ac2
frankie-eight-days <fkw155@mun.ca>
2017-09-10 21:17:22 -0230

    Added commenting.

--- googleSearches.py
+++ googleSearches.py
@@ -2,35 +2,41 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from google import google
 
+# Setting up and authorizing the backend with Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
 def main():
-    conservativeSearchKey = []
-    liberalSearchKey = []
-    matchingIndicesList = []
-
+    # Gets the string of buzzwords from Google Sheets
     conservativeSearchKey = fillSearchTerms('right')
     liberalSearchKey = fillSearchTerms('left')
 
+    # Creates list of indices where topics of headlines match
     matchingIndicesList = matchSearches(conservativeSearchKey, liberalSearchKey)
 
+    # Then does the searches and updates the second sheet of the workbook
     doSearches(conservativeSearchKey, liberalSearchKey, matchingIndicesList)
 
 def doSearches(conservativeSearch, liberalSearch, indicesList):
+    # Open workbook and create worksheet (wks) object
     worksheet = client.open_by_key('1DTDmpP9dlUfZhfm8Bb4AL7-PfqlbjoCEI0FxUFGfjA0')
     wks = worksheet.worksheet('Sheet3')
-    rowNumber = 382 - 4
 
-    for i in range(75, len(indicesList)):
+    # Starts from one since there is no 0 cell or column in sheets
+    rowNumber = 1
 
+    for i in range(len(indicesList)):
+        # Creates cells containing buzzwords that will be searched
         wks.update_cell(i + 1 + rowNumber, 1, conservativeSearch[indicesList[i][0]])
         wks.update_cell(i + 1 + rowNumber, 4, liberalSearch[indicesList[i][1]])
 
+        # Searches google for the buzzwords
+        # CAUTION: if more than 45 searches are conducted, your IP will be temp banned from Google
         cSearchResults = google.search(conservativeSearch[indicesList[i][0]])
         lSearchResults = google.search(liberalSearch[indicesList[i][1]])
 
+        # Prints results from each search into it's appropriate cell
         for result in range(4):
             wks.update_cell(i+1+rowNumber, 2, cSearchResults[result].name)
             wks.update_cell(i + 1 + rowNumber, 5, lSearchResults[result].name)
@@ -39,24 +45,28 @@ def doSearches(conservativeSearch, liberalSearch, indicesList):
 
 
 def matchSearches(conservativeKeyList, liberalKeyList):
+    # Lists where key words and matching indices will be held
     keyWordsC = []
     keyWordsL = []
-
     matchingIndices = []
 
+    # Buzzword strings are broken back into words
     for i in range(len(conservativeKeyList)):
         keyWordsC.append(conservativeKeyList[i].split())
     for i in range(len(liberalKeyList)):
         keyWordsL.append(liberalKeyList[i].split())
 
+    # Searches through the conservative list looking for words that match in the liberal list
     for cHeadIndex in range(len(keyWordsC)):
         for cWordIndex in range(len(keyWordsC[cHeadIndex])):
             for lHeadIndex in range(len(keyWordsL)):
                 for lWordIndex in range(len(keyWordsL[lHeadIndex])):
+                    # If one word matches, the string index is matched and stored
                     if keyWordsC[cHeadIndex][cWordIndex] == keyWordsL[lHeadIndex][lWordIndex]:
                         matchingIndices.append([cHeadIndex, lHeadIndex])
     return matchingIndices
 
+# Grabs the buzzwords from the first sheet
 def fillSearchTerms(wing):
     sheet = client.open('googleSearchTerms').sheet1
     if wing == 'right':
--- headlines.py
+++ headlines.py
@@ -3,15 +3,15 @@ import requests
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+# Setting up and authorizing the backend with Google Drive API
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
 def main():
+    # Columns where information is stored on sheet one
     conservativeCol = 1
     liberalCol = 3
-
-    headlines = []
 
     conservativeNewsUrls = ['http://feeds.foxnews.com/foxnews/politics',
                             'http://feeds.feedburner.com/breitbart',
@@ -24,26 +24,26 @@ def main():
                        'https://rss.msn.com/en-ca/']
 
     for i in range(len(conservativeNewsUrls)):
-        headlines = []
-        getNews(conservativeNewsUrls[i], headlines)
-        keyWords = findKeyWords(headlines)
-        sheetAddKeyWords(keyWords, conservativeCol)
+        headlines = []                              # Clears headlines list
+        getNews(conservativeNewsUrls[i], headlines) # Gets the headlines from RSS feeds and stores in list
+        keyWords = findKeyWords(headlines)          # Extracts the buzzwords from the headlines
+        sheetAddKeyWords(keyWords, conservativeCol) # Then adds them on the first sheet column 1
 
     for i in range(len(liberalNewsUrls)):
-        headlines = []
-        getNews(liberalNewsUrls[i], headlines)
-        keyWords = findKeyWords(headlines)
-        sheetAddKeyWords(keyWords, liberalCol)
+        headlines = []                              # Clears headlines list
+        getNews(liberalNewsUrls[i], headlines)      # Gets headlines from RSS feeds and stores in list
+        keyWords = findKeyWords(headlines)          # Extracts the buzzwords from the headlines
+        sheetAddKeyWords(keyWords, liberalCol)      # Then adds them on the first sheet column 3
 
 def sheetAddKeyWords(keyWords, colNumber):
-    sheet = client.open('googleSearchTerms').sheet1
-    allDataCells = sheet.col_values(colNumber)
+    sheet = client.open('googleSearchTerms').sheet1 # Creates sheet object
+    allDataCells = sheet.col_values(colNumber)      # Grabs all the data in the passed in column
     rowCount = 0
-    for i in range(len(allDataCells)):
-        if not allDataCells[i] == '':
-            rowCount += 1
+    for i in range(len(allDataCells)):     # Loops through all the headlines
+        if not allDataCells[i] == '':      # Ignores blank cells
+            rowCount += 1                  # Counts upto where the blank cells begin
 
-    for words in range(len(keyWords)):
+    for words in range(len(keyWords)):     # Updates the cells after preexisting cells
         sheet.update_cell(words+1+rowCount, colNumber, keyWords[words])
 
 def findKeyWords(newsHeadlines):
@@ -52,36 +52,48 @@ def findKeyWords(newsHeadlines):
     numberOfHeadlines = 0
 
     for i in range(len(newsHeadlines)):
-        words = newsHeadlines[i].split()
-        for word in range(len(words)):
+        words = newsHeadlines[i].split()        # Each headline gets split into words (now have 2d list)
+        for word in range(len(words)):          # Loops through each word in headline
             if len(words[word]) > 5:
-                keyWords.append(words[word])
-        keyWords.append('.')
+                keyWords.append(words[word])    # Word is added to master list of buzzwords
+        keyWords.append('.')                    # Period differentiates what keyword belongs to what headline
         numberOfHeadlines += 1
 
         currentHeadline = ''
 
-    for i in range(len(keyWords)):
-         if not keyWords[i] == '.':
-            currentHeadline += keyWords[i]
+    for i in range(len(keyWords)):                  # Loops through all the words now
+         if not keyWords[i] == '.':                 # Checks to see if words belong together
+            currentHeadline += keyWords[i]          # If they do, they get added to a temporary str
             currentHeadline += ' '
          else:
-            keyWordStrings.append(currentHeadline)
+            keyWordStrings.append(currentHeadline)  # When a period is found, the str gets appended to master list
             currentHeadline = ''
 
     return keyWordStrings
 
 
 def getNews(url, arrayOfNews):
+    # Gets the raw data from website
     response = requests.get(url)
+
+    # Converts the raw data to string, then ElementTree is used to find the elements
     stringData = response.text
     tree = ElementTree.fromstring(stringData)
 
+    # All RSS feeds follow the same path to get to headlines
+    # There are three layers that must be penetrated, the path will always be: 'rss/channel/item'
+    # Hence the three nested for loops below
+    # First for loop opens the rss element
     for child in tree:
+        # Second loop opens the channel element
         for doubleChild in child:
+            # Inside channel element there ~10 elements describing the feed
+            # If statement is used to filter them (Something more elegant could probably be done with ET)
             if doubleChild.tag == 'item':
+                # Third for loop filters through the elements in each article for the headline
                 for tripleChild in doubleChild:
                     if tripleChild.tag == 'title':
+                        # Finally the headline is appended to it's array
                         arrayOfNews.append(tripleChild.text)
 
 main()
